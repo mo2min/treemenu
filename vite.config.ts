ded9b3e63051c8ae2f9b6c3c0837f0b4c769604b
mo2min <fireb1003@googlemail.com>
2022-04-14 12:29:53 +0000

    buildo

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from "vite-dts";
-import * as path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-        entry: path.resolve(__dirname, 'src/index.tsx'),
-        name: 'TreeMenu',
-        formats: ['es', 'umd'],
-        fileName: (format) => `tree-menu.${format}.js`,
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "TreeMenu",
+      formats: ["es", "umd"],
+      fileName: (format) => `tree-menu.${format}.js`,
     },
     rollupOptions: {
-        external: ['react', 'react-dom'],
-        output: {
-            globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-            },
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
         },
+      },
     },
-},
-})
+  },
+});
